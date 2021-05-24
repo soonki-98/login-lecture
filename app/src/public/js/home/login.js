d8bd184cb2e4aec38248f55e8d10857c798ec1f9
Soonki <alstnsrl98@gmail.com>
2021-05-24 14:57:48 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -9,4 +9,12 @@ loginBtn.addEventListener("click", login => {
         id: id.value,
         pw: pw.value,
     };
+    
+    fetch("/login", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req),
+    });
 })
