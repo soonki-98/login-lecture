48ba33e763ff62e7fa9f7e3ca16e22ea105599c5
Soonki <alstnsrl98@gmail.com>
2021-05-26 14:30:42 +0900

    유저 module구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const User = require("../../modules/User");
 const UserStorage = require("../../modules/UserStorage");
 
 const output = {
@@ -14,21 +15,8 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id", "psword");
-        const response = {};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        
-        response.success = false;
-        response.msg = "로그인에 실패하였습니다.";
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     },
 };
@@ -37,3 +25,4 @@ module.exports = {
     output,
     process,
 };
+
